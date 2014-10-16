ea02aa279663167223584a8a22027bbb9052dcd2
Jin Tian <Today.xa@gmail.com>
2014-10-16 21:33:21 +0800

    运行时显示黑窗口

--- c.bat
+++ c.bat
@@ -2,4 +2,4 @@
 @del top200_ping.exe
 
 @echo [Compile Script to .exe]
-"C:\Program Files\AutoIt3\Aut2Exe\Aut2exe.exe" /in findCo.au3 /out findCo.exe /icon .\favicon.ico /nodecompile /comp 4
+"C:\Program Files\AutoIt3\Aut2Exe\Aut2exe.exe" /in findCo.au3 /out bin\findCo.exe /icon .\favicon.ico /console /comp 1
